d86a5109ab50451dc5f1e688969953abf99d982b
ThenuKalyan <thenammainachiyappan@ThenuKalyans-MacBook-Air.local>
2025-08-15 10:56:42 -0700

    uploading the app

--- app/src/App.js
+++ app/src/App.js
@@ -1,30 +1,52 @@
-import logo from './logo.svg';
+
 import './App.css';
+import rajiniQuotes from './quotes'
+import { useState, useEffect } from 'react'
+import leftQuote from './quote-left.png'
+import rightQuote from './right.png'
+import twitter from './twitter.png'
+import tumblr from './tumblr.png'
+
+const App = () => {
+  const [quotes, setQuotes] = useState("மலை டா… அண்ணாமலை.")
+  const [randomNumber, setRandomNumber] = useState(0)
+  const colors = ["red", "blue", "green", "purple", "orange"];
+  const [color, setColor] = useState("black");
+  useEffect(() => {
+
+  }, [quotes])
+
+
+  const handleClick = (e) => {
+    e.preventDefault();
+    const randomIndex = Math.floor(Math.random() * rajiniQuotes.length);
+    const randomColor = colors[Math.floor(Math.random() * colors.length)];
+    setColor(randomColor);
+    setQuotes(rajiniQuotes[randomIndex]);
+  }
 
 
 
-function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        {/* <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a> */}
-        <div id="quote-box">
-          <label>
-      
-            <textarea name="quotesBox" rows={4} cols={40} />
-          </label>
+      <header className="App-header" style={{ backgroundColor: color}}>
+        <div id="quote-box" style={{ color: color }} rows={4} cols={40} >
 
+          <p id="text">
+            <img className="img" src={leftQuote} />
+            {quotes}
+            <img className="img" src={rightQuote} />
+          </p>
+          <p id="author">- LavaGirl
+
+          </p>
+          <span className='links'>
+            <button id="new-quote" style={{ backgroundColor: color, border:color}}className="btn btn-primary" onClick={(e) => handleClick(e)}>new-Quote</button>
+            <div className='socialLinks' >
+              <a href="https://twitter.com/intent/tweet" style={{ color: color }} id="tweet-quote"> <img className="img" src={twitter} /></a>
+              <a href="https://tumblr.com/" style={{ color: color }} id="tumblr"> <img className="img" src={tumblr} /></a>
+            </div>
+          </span>
         </div>
       </header>
     </div>
